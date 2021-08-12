92aa0742e936bd2d40decef24c229a0db9d84d4e
Mohamed Nasser <mohamed.salaheldienhamed@vodafone.com>
2021-08-12 14:47:54 +0200

    minor update

--- NewsApp/Utils/RequestConstants.swift
+++ NewsApp/Utils/RequestConstants.swift
@@ -8,8 +8,8 @@
 import Foundation
 
 enum RequestConstants {
-    static let apiKey = "339101e1a9da4f71925f0bd2e4b8bdf4"
     static let baseURL = "https://newsapi.org/v2/"
+    static let apiKey = ""
 }
 
 enum UIConstants {
