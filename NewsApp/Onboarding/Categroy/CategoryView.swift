35cc190c19531654a2a421bec6ec8e2c9de66140
Mohamed Nasser <mohamed.salaheldienhamed@vodafone.com>
2021-08-13 17:32:29 +0200

    Enhance category selection logic

--- NewsApp/Onboarding/Categroy/CategoryView.swift
+++ NewsApp/Onboarding/Categroy/CategoryView.swift
@@ -11,22 +11,22 @@ class CategoryView: UIView {
     @IBOutlet weak var categoryNameLabel: UILabel!
     @IBOutlet weak var isSelectedImageView: UIImageView!
     weak var delegate: CategoryViewDelegate?
+    var isSelected: Bool = false {
+        didSet {
+            isSelectedImageView.tintColor = isSelected ? .checkBoxSelectedColor : .checkBoxUnSelectedColor
+        }
+    }
 
     func setup(name: String, delegate: CategoryViewDelegate?) {
         categoryNameLabel.text = name.localized
         self.delegate = delegate
         isSelectedImageView.image = isSelectedImageView.image?.withRenderingMode(.alwaysTemplate)
-        isSelectedImageView.tintColor = .checkBoxUnSelectedColor
+        isSelected = false
     }
 
     @IBAction func viewDidPress(_ sender: UIButton) {
-        let isSelected =  isSelectedImageView.tintColor == .checkBoxSelectedColor
         if !(delegate?.isUpdateAllowed(isSelected: isSelected) ?? false) { return }
-        if isSelected {
-            isSelectedImageView.tintColor = .checkBoxUnSelectedColor
-        } else {
-            isSelectedImageView.tintColor = .checkBoxSelectedColor
-        }
+        isSelected.toggle()
         delegate?.categoryDidPress(
             name: categoryNameLabel.text ?? "",
             isSelected: !isSelectedImageView.isHidden)
